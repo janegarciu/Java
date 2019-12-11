853a307d2b6bb1f50f82eb3da1976a0ef84e8972
Eugenia <eugenia.garciu@isa.utm.md>
2019-12-11 17:14:09 +0200

    Update

--- src/com/jenika/oop/Lab1/task1/Main.java
+++ src/com/jenika/oop/Lab1/task1/Main.java
@@ -1,11 +1,11 @@
-package Lab1.task1;
+package com.jenika.oop.Lab1.task1;
 
 public class Main {
 
 
         public static void main(String[] args) {
-            Monitor mon1 = new Monitor();
-            Monitor mon2 = new Monitor();
+            Lab1.task1.Monitor mon1 = new Lab1.task1.Monitor();
+            Lab1.task1.Monitor mon2 = new Lab1.task1.Monitor();
             mon1.color = "White";
             mon2.color = "Blue";
             mon1.dimension = 24;
@@ -14,7 +14,7 @@ public class Main {
             mon2.resolution = 17;
             System.out.println(mon1.color+" "+mon1.dimension+" "+mon1.resolution);
             System.out.println(mon2.color+" "+mon2.dimension+" "+mon2.resolution);
-            Comparator comparator = new Comparator();
+            Lab1.task1.Comparator comparator = new Lab1.task1.Comparator();
             comparator.compare(mon1.dimension, mon2.dimension);
             comparator.compare(mon1.resolution, mon2.resolution);
         }
